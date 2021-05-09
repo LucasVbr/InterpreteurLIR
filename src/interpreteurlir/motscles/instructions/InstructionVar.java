8cee23eb79ce29af4e3473258c201b059b8c59a8
Pierre Debas <dpierre394@gmail.com>
2021-05-09 11:30:28 +0200

    instruction / instruction var

---
+++ src/interpreteurlir/motscles/instructions/InstructionVar.java
@@ -0,0 +1,57 @@
+/**
+ * InstructionVar.java                                             9 mai 2021
+ * IUT info1 2020-2021, pas de copyright, aucun droit
+ */
+package interpreteurlir.motscles.instructions;
+
+import interpreteurlir.Contexte;
+import interpreteurlir.InterpreteurException;
+import interpreteurlir.expressions.Expression;
+
+/**
+ * Instruction de déclaration et d'affectation de variables. La syntaxe de
+ * cette expression est de la forme var identificateur = expression. Si
+ * expression non renseignée, l'interpréteur affichera un message d'erreur ;
+ * l'instruction doit effectuer systématiquement une affectation.
+ * @author Nicolas Caminade
+ * @author Sylvan Courtiol
+ * @author Pierre Debas
+ * @author Heïa Dexter
+ * @author Lucas Vabre
+ */
+public class InstructionVar extends Instruction {
+
+    /**
+     * TODO comment initial state of the object
+     * @param arguments expression à exécuter
+     * @param contexte global de l'interpréteur
+     */
+    public InstructionVar(String arguments, Contexte contexte) {
+        super(arguments, contexte);
+        
+        if (arguments == null || arguments.isBlank() 
+                || arguments.indexOf('=') < 0)
+            throw new InterpreteurException("erreur de syntaxe");
+        
+        aExecuter = Expression.determinerTypeExpression(arguments.trim());
+    }
+
+    /*
+     * Non - javadoc
+     * @see interpreteurlir.motscles.instructions.Instruction#executer()
+     */
+    @Override
+    public boolean executer() {
+        aExecuter.calculer();
+        return true;
+    }
+
+    /*
+     * Non - javadoc
+     * @see interpreteurlir.motscles.instructions.Instruction#toString()
+     */
+    @Override
+    public String toString() {       
+        return "var " + aExecuter;
+    }
+}
