de402570356d2fcdc93966ffba610bf3fda70d02
LaNymphtaline <80621927+LaNymphtaline@users.noreply.github.com>
2021-05-23 11:56:51 +0200

    Harmonisation TestChaine

--- src/interpreteurlir/donnees/litteraux/tests/TestChaine.java
+++ src/interpreteurlir/donnees/litteraux/tests/TestChaine.java
@@ -10,142 +10,127 @@ import interpreteurlir.donnees.litteraux.Chaine;
 import static info1.outils.glg.Assertions.*;
 
 /**  
- * Tests unitaires de Chaine
+ * Tests unitaires de la classe Chaine
+ * 
  * @author Nicolas Caminade
  * @author Sylvan Courtiol
  * @author Pierre Debas
- * @author Heïa Dexter
+ * @author HeÃ¯a Dexter
  * @author Lucas Vabre
  */
 public class TestChaine {
-	
-	
-	/** test de Chaine(String) */
-	public static void testChaine() {
-		
-		final String[] VALIDE = { 
-			"\"arztyehjklmpoijhghnbghjklmpoiuytrf" + 
-			"ghjnklmpoiuytrezaqsdfghnjklmpjbfrtyu\"", 
-			"\"\"","\"coucou \"",
-			"\"" + 42 + "\""
-		};  
-		
-	    final String INVALIDE =
-			"arztyehjklmpoijhghnbghjklmpoiuytrf" + 
-			"yeryghjnklmpoiuytrezaqsdfghnjklmpjbfrtyu";
 
-	    System.out.println("\ttest de Chaine(String)");
-	    
-	    for (String aTester : VALIDE)
-	    	new Chaine(aTester);
-	    
-	    try {
-	    	new Chaine(INVALIDE);
-	    	throw new RuntimeException("Instanciation interdite");
-	    } catch (InterpreteurException lancee) {
-	    	System.out.println("\tRevoi d'exception OK\nfin du test");
-	    }
-	}
-	
-	/** test  de compareTo */
-	public static void testCompareTo() {
-		final Chaine[][] EGALITES = { 
-			{new Chaine("\"coucou\""), new Chaine("\"coucou\"")}, 
-			{new Chaine("\" \""), new Chaine("\" \"")},
-			{new Chaine("\"\""), new Chaine()} 
-		};
-		
-		final Chaine[][] DIFFERENCES = { 
-			{new Chaine("\"coucou\""), new Chaine("\"camomille\"")},
-			{new Chaine("\"tarentule\""), new Chaine("\"coucou\"")}, 
-			{new Chaine("\"coucou\""), new Chaine("\" \"")}, 
-			{new Chaine("\"coucou\""), new Chaine()},
-			{new Chaine("\" \""), new Chaine()} 
-		};
-		
-		System.out.println("\ttest de compareTo(Chaine)\nAvec égalités");
-		
-		for (Chaine[] couple : EGALITES) {
-			
-			try {
-		    	assert couple[0].compareTo(couple[1]) == 0;
-		    } catch (AssertionError lancee) {
-				echec();
-		    }	
-			
-		}
-		
-		System.out.println("\tAvec des inégalités");
-		for (Chaine[] couple : DIFFERENCES) {
-			try {
-		    	assert couple[0].compareTo(couple[1]) > 0;
-		    } catch (AssertionError lancee) {
-				System.err.println("Echec du test");
-		    }
-		}
-		System.out.println("\tfin du test");
-	
-	}
-	
-	/** test de toString */
-	public static void testToString() {
-		final Chaine[] A_AFFICHER = {
-			new Chaine(), new Chaine("\" \""), 
-			new Chaine("\"coucou\""),
-			new Chaine("\" coucou \""), 
-			new Chaine("\"coucou monsieur\"")
-		};
-		
-		final String[] AFFICHAGE_GUILLEMETS = {
-				"\"\"", "\" \"", "\"coucou\"", "\" coucou \"",
-				"\"coucou monsieur\""
-		};
-		
-		System.out.println("\ttest de toString");
-		for (int i = 0 ; i < A_AFFICHER.length ; i++) {
-			
-			try {
-				assert A_AFFICHER[i].toString().equals(AFFICHAGE_GUILLEMETS[i]);
-			} catch (AssertionError lancee) {
-				System.err.println("Echec du test a l'indice " + i);
-			}
-		}
-		System.out.println("\t==>test terminé\n");		
-	}
-	
-	/**
-	 * Tests unitaires de concaténer
-	 */
-	public void testConcatener() {
-	    final Chaine[] ATTENDU = {
-	        new Chaine(),
-	        new Chaine("\"Bonjour le monde ! \""),
-	        new Chaine("\" \""),
-	        new Chaine("\"3,1415\""),
-	    };
-	    
-	    final Chaine[][] A_CONCATENER = {
-	        {new Chaine(), new Chaine("\"\"")},   
-	        {new Chaine("\"Bonjour \""), new Chaine("\"le monde ! \"")},
-	        {new Chaine("\"\""), new Chaine("\" \"")},
-	        {new Chaine("\"3,\""), new Chaine("\"1415\"")},
-	    };
-	    
-	    System.out.println("\tExécution du test de concaténer");
-	    for (int numTest = 0 ; numTest < ATTENDU.length ; numTest++ ) {
-	        assertTrue(ATTENDU[numTest].compareTo(Chaine.concatener(
-	                   A_CONCATENER[numTest][0], A_CONCATENER[numTest][1])) 
-	                   == 0);
-	    }
-	}
-	
-	/** 
-	 * Lancement des tests
-	 * @param args non utilisés
-	 */
-	public static void main(String[] args) {
-		testChaine();
-		testCompareTo();
-		testToString();
-	}
+
+    /** Test unitaire de {@link Chaine#Chaine(String)} */
+    public static void testChaine() {
+
+        final String[] VALIDE = { 
+                "\"arztyehjklmpoijhghnbghjklmpoiuytrf" + 
+                        "ghjnklmpoiuytrezaqsdfghnjklmpjbfrtyu\"", 
+                "\"\"",
+                "\"coucou \"",
+                "\"" + 42 + "\""
+        };  
+
+        final String INVALIDE =
+                "arztyehjklmpoijhghnbghjklmpoiuytrf" + 
+                        "yeryghjnklmpoiuytrezaqsdfghnjklmpjbfrtyu";
+
+        System.out.println("\tExÃ©cution du test de Chaine(String)");
+
+        for(String aTester : VALIDE) {
+            try {
+                new Chaine(aTester);
+            } catch (InterpreteurException lancee) {
+                echec();
+            }
+        }
+
+        try {
+            new Chaine(INVALIDE);
+            echec();
+        } catch (InterpreteurException lancee) {
+            // Test OK
+        }
+    }
+
+    /** Test unitaire de {@link Chaine#compareTo(Litteral)} */
+    public static void testCompareTo() {
+        final Chaine[][] EGALITES = { 
+                { new Chaine("\"coucou\""), new Chaine("\"coucou\"") }, 
+                { new Chaine("\" \""), new Chaine("\" \"") },
+                { new Chaine("\"\""), new Chaine() } 
+        };
+
+        final Chaine[][] DIFFERENCES = { 
+                { new Chaine("\"coucou\""), new Chaine("\"camomille\"") },
+                { new Chaine("\"tarentule\""), new Chaine("\"coucou\"") }, 
+                { new Chaine("\"coucou\""), new Chaine("\" \"") }, 
+                { new Chaine("\"coucou\""), new Chaine() },
+                { new Chaine("\" \""), new Chaine() } 
+        };
+
+        System.out.println("\tExÃ©cution du test de compareTo(Chaine)\n"
+                            + "\tAvec Ã©galitÃ©s");
+
+        for (Chaine[] couple : EGALITES) {
+            assertEquivalence(couple[0].compareTo(couple[1]), 0);
+        }
+
+        System.out.println("\tAvec des inÃ©galitÃ©s");
+        for (Chaine[] couple : DIFFERENCES) {
+            assertTrue(couple[0].compareTo(couple[1]) > 0);
+        }
+    }
+
+    /** Test unitaire de {@link Chaine#toString()} */
+    public static void testToString() {
+        final Chaine[] A_AFFICHER = {
+                new Chaine(), 
+                new Chaine("\" \""), 
+                new Chaine("\"coucou\""),
+                new Chaine("\" coucou \""), 
+                new Chaine("\"coucou monsieur\"")
+        };
+
+        final String[] AFFICHAGE_GUILLEMETS = {
+                "\"\"", 
+                "\" \"", 
+                "\"coucou\"", 
+                "\" coucou \"",
+                "\"coucou monsieur\""
+        };
+
+        System.out.println("\tExÃ©cution du test de toString");
+        for (int i = 0 ; i < A_AFFICHER.length ; i++) {
+            assertTrue(AFFICHAGE_GUILLEMETS[i]
+                        .equals(A_AFFICHER[i].toString()));
+        }
+    }
+
+    /**
+     * Tests unitaires de concatÃ©ner
+     */
+    public static void testConcatener() {
+        final Chaine[] ATTENDU = {
+                new Chaine(),
+                new Chaine("\"Bonjour le monde ! \""),
+                new Chaine("\" \""),
+                new Chaine("\"3,1415\""),
+        };
+
+        final Chaine[][] A_CONCATENER = {
+                { new Chaine(), new Chaine("\"\"") },   
+                { new Chaine("\"Bonjour \""), new Chaine("\"le monde ! \"") },
+                { new Chaine("\"\""), new Chaine("\" \"") },
+                { new Chaine("\"3,\""), new Chaine("\"1415\"") },
+        };
+
+        System.out.println("\tExÃ©cution du test de concatÃ©ner");
+        for (int numTest = 0 ; numTest < ATTENDU.length ; numTest++ ) {
+            assertTrue(ATTENDU[numTest].compareTo(
+                           Chaine.concatener(A_CONCATENER[numTest][0], 
+                                   A_CONCATENER[numTest][1])) == 0);
+        }
+    }
 }
