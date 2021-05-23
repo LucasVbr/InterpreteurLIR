256df47153df86225315e363b8818612519a887c
LaNymphtaline <80621927+LaNymphtaline@users.noreply.github.com>
2021-05-23 11:51:29 +0200

    Harmonisation TestChaine

--- src/interpreteurlir/donnees/tests/TestIdentificateurChaine.java
+++ src/interpreteurlir/donnees/tests/TestIdentificateurChaine.java
@@ -1,94 +1,136 @@
-/*
- * TestIdentificateurChaine.java, 08/05/2021
- * IUT Rodez 2020-2021, info1
- * pas de copyright, aucun droits
+/**
+ * TestChaine.java                                    8 mai 2021
+ * IUT info1 2020-2021, pas de copyright, aucun droit
  */
+package interpreteurlir.donnees.litteraux.tests;
 
-package interpreteurlir.donnees.tests;
+import interpreteurlir.InterpreteurException;
+import interpreteurlir.donnees.litteraux.Chaine;
 
 import static info1.outils.glg.Assertions.*;
 
-import interpreteurlir.InterpreteurException;
-import interpreteurlir.donnees.IdentificateurChaine;
-
-/**
- * Tests unitaires de la classe donnees.IdentificateurEntier
+/**  
+ * Tests unitaires de la classe Chaine
+ * 
  * @author Nicolas Caminade
  * @author Sylvan Courtiol
  * @author Pierre Debas
- * @author Heia Dexter
+ * @author Heïa Dexter
  * @author Lucas Vabre
  */
-public class TestIdentificateurChaine {
-    
-    /** Jeu d'identificateurs de cha�ne correctement instanci�s */
-    private static IdentificateurChaine[] FIXTURE = {
-            new IdentificateurChaine("$a"),
-            new IdentificateurChaine("$A"),
-            new IdentificateurChaine("$alpha"),
-            new IdentificateurChaine("$Alpha"),
-            new IdentificateurChaine("$Alpha5"),
-            new IdentificateurChaine("$jeSuisUnTresLongIdentifi")
-    };
+public class TestChaine {
 
-    /**
-     * Tests unitaires du constructeur IdentificateurEntier(String identificateur)
-     */
-    public static void testIdentificateurChaineString() {
-        final String[] INVALIDE = {
-                "",
 
-                // Commence par une lettre
-                "9alpha",
-                "  5alpha",
+    /** Test unitaire de {@link Chaine#Chaine(String)} */
+    public static void testChaine() {
 
-                // Fait au maximum 25 caract�res
-                "$jeSuisUnTresLongIdentificateur", // 30 char
-                "$jeSuisUnTresLongIdentifica",
+        final String[] VALIDE = { 
+                "\"arztyehjklmpoijhghnbghjklmpoiuytrf" + 
+                        "ghjnklmpoiuytrezaqsdfghnjklmpjbfrtyu\"", 
+                "\"\"",
+                "\"coucou \"",
+                "\"" + 42 + "\""
+        };  
 
-                // Espaces
-                "id 3a",
-                "$id 3a",
-                " ",
-                "$ ",
+        final String INVALIDE =
+                "arztyehjklmpoijhghnbghjklmpoiuytrf" + 
+                        "yeryghjnklmpoiuytrezaqsdfghnjklmpjbfrtyu";
 
-                // caract�res d'�chapements
-                "\t",
-                "\n",
-                "$\t",
-                "$\n",
+        System.out.println("\tExécution du test de Chaine(String)");
 
-                // , cas particulier
-                "$",
-                "$1"
-        };
-
-        for(int noJeu = 0; noJeu < INVALIDE.length ; noJeu++) {
+        for(String aTester : VALIDE) {
             try {
-                new IdentificateurChaine(INVALIDE[noJeu]);
-                echec();
+                new Chaine(aTester);
             } catch (InterpreteurException lancee) {
-                // test OK
+                echec();
             }
         }
-        
+
+        try {
+            new Chaine(INVALIDE);
+            echec();
+        } catch (InterpreteurException lancee) {
+            // Test OK
+        }
+    }
+
+    /** Test unitaire de {@link Chaine#compareTo(Litteral)} */
+    public static void testCompareTo() {
+        final Chaine[][] EGALITES = { 
+                { new Chaine("\"coucou\""), new Chaine("\"coucou\"") }, 
+                { new Chaine("\" \""), new Chaine("\" \"") },
+                { new Chaine("\"\""), new Chaine() } 
+        };
+
+        final Chaine[][] DIFFERENCES = { 
+                { new Chaine("\"coucou\""), new Chaine("\"camomille\"") },
+                { new Chaine("\"tarentule\""), new Chaine("\"coucou\"") }, 
+                { new Chaine("\"coucou\""), new Chaine("\" \"") }, 
+                { new Chaine("\"coucou\""), new Chaine() },
+                { new Chaine("\" \""), new Chaine() } 
+        };
+
+        System.out.println("\tExécution du test de compareTo(Chaine)\n"
+                            + "\tAvec égalités");
+
+        for (Chaine[] couple : EGALITES) {
+            assertEquivalence(couple[0].compareTo(couple[1]), 0);
+        }
+
+        System.out.println("\tAvec des inégalités");
+        for (Chaine[] couple : DIFFERENCES) {
+            assertTrue(couple[0].compareTo(couple[1]) > 0);
+        }
+    }
+
+    /** Test unitaire de {@link Chaine#toString()} */
+    public static void testToString() {
+        final Chaine[] A_AFFICHER = {
+                new Chaine(), 
+                new Chaine("\" \""), 
+                new Chaine("\"coucou\""),
+                new Chaine("\" coucou \""), 
+                new Chaine("\"coucou monsieur\"")
+        };
+
+        final String[] AFFICHAGE_GUILLEMETS = {
+                "\"\"", 
+                "\" \"", 
+                "\"coucou\"", 
+                "\" coucou \"",
+                "\"coucou monsieur\""
+        };
+
+        System.out.println("\tExécution du test de toString");
+        for (int i = 0 ; i < A_AFFICHER.length ; i++) {
+            assertTrue(AFFICHAGE_GUILLEMETS[i]
+                        .equals(A_AFFICHER[i].toString()));
+        }
     }
 
     /**
-     * Tests unitaires de getNom()
+     * Tests unitaires de concaténer
      */
-    public static void testGetNom() {
-        final String[] NOM_VALIDES = {
-                "$a",
-                "$A",
-                "$alpha",
-                "$Alpha",
-                "$Alpha5",
-                "$jeSuisUnTresLongIdentifi"
+    public static void testConcatener() {
+        final Chaine[] ATTENDU = {
+                new Chaine(),
+                new Chaine("\"Bonjour le monde ! \""),
+                new Chaine("\" \""),
+                new Chaine("\"3,1415\""),
         };
 
-        for (int noJeu = 0 ; noJeu < NOM_VALIDES.length ; noJeu++) {
-            assertEquivalence(NOM_VALIDES[noJeu], FIXTURE[noJeu].getNom());
+        final Chaine[][] A_CONCATENER = {
+                { new Chaine(), new Chaine("\"\"") },   
+                { new Chaine("\"Bonjour \""), new Chaine("\"le monde ! \"") },
+                { new Chaine("\"\""), new Chaine("\" \"") },
+                { new Chaine("\"3,\""), new Chaine("\"1415\"") },
+        };
+
+        System.out.println("\tExécution du test de concaténer");
+        for (int numTest = 0 ; numTest < ATTENDU.length ; numTest++ ) {
+            assertTrue(ATTENDU[numTest].compareTo(
+                           Chaine.concatener(A_CONCATENER[numTest][0], 
+                                   A_CONCATENER[numTest][1])) == 0);
         }
     }
 }
